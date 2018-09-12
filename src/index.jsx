9ce2925fe839cd214ff8025ee898ca14346e66b6
mariya <mhozyaenko@gmail.com>
2018-09-13 00:05:36 +0300

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './assets/components/app.js';
+
+import './index.scss';
+
+ReactDOM.render(<App/>, document.querySelector('#app'));
+
