3188f8c6e7cf845dd76c8ff9be211e2e18540a7d
mariya <mhozyaenko@gmail.com>
2018-09-19 23:47:19 +0300

    styleguidist

--- webpack.config.js
+++ webpack.config.js
@@ -5,76 +5,74 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: './src/index.jsx',
-  output: {
-    path: path.join(process.cwd(), 'dist'),
-    filename: '[name].js'
-  },
-  module: {
-    rules: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader'
-      }
-    }, {
-      test: /\.(scss|css)$/,
-      use: [{
-        loader: isProduction ? MiniCssExtractPlugin.loader : 'style-loader'
-      }, {
-        loader: 'css-loader',
-        options: { sourceMap: !isProduction }
-      }, {
-        loader: 'sass-loader',
-        options: { sourceMap: !isProduction }
-      }, {
-        loader: 'sass-resources-loader',
-        options: {
-          resources: [
-            './src/assets/styles/_resources.scss'
-          ]
-        }
-      }]
+    entry: './src/index.jsx',
+    output: {
+        path: path.join(process.cwd(), 'dist'),
+        filename: '[name].js'
     },
-    {
-      test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
-      use: [{
-         loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: './fonts/'
-        }
-      }]
-    }, {
-      test: /\.(png|jpe?g|gif)$/,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'img/'
-        }
-      }]
-    }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Webpack Sandbox',
-      filename: 'index.html',
-      chunks: ['main'],
-      template: './src/assets/templates/index.html',
-      inject: true
-    })
-  ],
-  devServer: {
-    contentBase: path.join(process.cwd(), 'dist'),
-    compress: true,
-    port: 3000
-  },
-    // components: './src/components/**/*.{js,jsx}',
-    // webpackConfig: require('./webpack.config.js')
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }, {
+            test: /\.(scss|css)$/,
+            use: [{
+                loader: isProduction ? MiniCssExtractPlugin.loader : 'style-loader'
+            }, {
+                loader: 'css-loader',
+                options: {sourceMap: !isProduction}
+            }, {
+                loader: 'sass-loader',
+                options: {sourceMap: !isProduction}
+            }, {
+                loader: 'sass-resources-loader',
+                options: {
+                    resources: [
+                        './src/assets/styles/_resources.scss'
+                    ]
+                }
+            }]
+        },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: './fonts/'
+                    }
+                }]
+            }, {
+                test: /\.(png|jpe?g|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'img/'
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Webpack Sandbox',
+            filename: 'index.html',
+            chunks: ['main'],
+            template: './src/assets/templates/index.html',
+            inject: true
+        })
+    ],
+    devServer: {
+        contentBase: path.join(process.cwd(), 'dist'),
+        compress: true,
+        port: 3000
+    },
 };
